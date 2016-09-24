1362a8bb833ec865c4a13d44c8c46f9e5494c1d8
PiIsLife314 <etownlifes@gmail.com>
2016-09-24 16:52:23 -0500

    cleaned some of my mess

--- index.php
+++ index.php
@@ -2,58 +2,51 @@
 
 session_start();
 
-$back = "id= 'homeB'";
-$title = "Cloud Storage";
-$name = "smith";
+
 $top= " <!DOCTYPE html>
 <html>
-<head>
-<title>$title</title>
+	<head>
+		<title>Cloud Storage</title>
 
-<link rel='icon' href='images/cloud.png'>
-<link rel='stylesheet' type='text/css' href='styles.css'>
-<script src='main.js'></script>
-</head>
-<body $back>";
+		<link rel='icon' href='images/cloud.png'>
+		<link rel='stylesheet' type='text/css' href='styles.css'>
+		<script src='main.js'></script>
+	</head>
+	<body id= 'homeB'>";
 
-//$_SESSION['id_user'] = 1;
-//$_SESSION['name_user'] = "Steve";
 
 if(isset($_SESSION['name_user']))
 {
 	//show the file storage for user
 	$name = $_SESSION['name_user'];
 	$body = "
-	<div id= 'headB'>	
-		<div class= 'right' id= 'userB' onclick='menu(\"menu\");'>
-			<div class= 'left centered' id= 'userMenu'>$name</div>
-			<img src= 'images/user.jpg' height='58'>			
+		<div id= 'headB'>	
+			<div class= 'right' id= 'userB' onclick='menu(\"menu\");'>
+				<div class= 'left centered' id= 'userMenu'>$name</div>
+				<img src= 'images/user.jpg' height='58'>			
+			</div>
+			<div class= 'right centered headMenu' onclick='group();'>Groups</div>
+			<div class= 'right centered headMenu' onclick='loadFiles(\"$name\");'>Personal</div>
+			<div id= 'titlebox'>
+				<div id= 'headTitle'>CLOUD STORAGE PROJECT</div>
+			</div>
+			<div id='menu'>
+				<div class='centered menuBox'>Upload Files</div>
+				<div class='centered menuBox'>Logout</div>
+			</div>
 		</div>
-		<div class= 'right centered headMenu' onclick='group();'>Groups</div>
-		<div class= 'right centered headMenu' onclick='loadFiles(\"$name\");'>Personal</div>
-		<div id= 'titlebox'>
-			<div id= 'headTitle'>CLOUD STORAGE PROJECT</div>
-		</div>
-		<div id='menu'>
-		<div class='centered menuBox'>Upload Files</div>
-		<div class='centered menuBox'>Logout</div>
-		</div>
-	</div>
-	
-		<div class='under' id='content'>
-			
-		</div>
-	
-		<form action='test2.php' method='get'>
-		<input type='text' name='lname'><br>
-  <input type='submit' value='Submit'>
-		</form>
 		
-	";
+			<div class='under' id='content'>
+				
+			</div>
+	
+		
+	</body>
+</html>	";
 	
 }
 
-else{/**/
+else{
 	//show the login & register page
 $body = "<div id='loginBox'>
 			<h1>Login</h1>
