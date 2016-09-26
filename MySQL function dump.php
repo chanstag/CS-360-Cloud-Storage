c26b60dbee9c5e5d99a10f11772270fcf3d5e336
Dustin Avery <Dustin Avery>
2016-09-26 14:21:33 -0500

    Added removing groups and users from groups to the function list

--- MySQL function dump.php
+++ MySQL function dump.php
@@ -1,5 +1,6 @@
 <?php
-//Makes a group including all the users in the array
+//Makes a group including all the users in the array, Can also be used to add multiple members
+//to a preexisting group
 function makeGroup($groupname, array $users){
 	for($i = 0; i < count($users); i++){
 		$host = "localhost:3036";
@@ -25,7 +26,7 @@ function makeGroup($groupname, array $users){
 	
 	
 }
-//Makes s group for one person, Can also be used to add a member to a group
+//Makes a group for one person, Can also be used to add a member to a preexisting group
 function makeGroup($groupname, string $username){
 		$host = "localhost:3036";
 		$user = "root";
@@ -43,7 +44,71 @@ function makeGroup($groupname, string $username){
 					" VALUES ('$groupname','$usersname')";
 			$conn->query($sql);
 			$conn->close();
+			return "Success";
 		}
+	
+}
+//Removes an entire group
+function removeGroup($groupName){
+		$host = "localhost:3036";
+		$user = "root";
+		$pass = "Ubuntu14.04";
+
+		$conn = new mysqli($host,$user,$pass,"bigreddocstorage");
+
+		if($conn->connect_error){
+			$conn->close;
+			return "No MySQL server";
+		}
+		else{
+			$sql = "DELETE FROM groups WHERE groupName = $groupName";
+			$conn->query($sql);
+			$conn->close();
+			return "Success";
+		}
+	
+}
+//Removes single user from group
+function removeUserFromGroup($groupName, string $username){
+		$host = "localhost:3036";
+		$user = "root";
+		$pass = "Ubuntu14.04";
+
+		$conn = new mysqli($host,$user,$pass,"bigreddocstorage");
+
+		if($conn->connect_error){
+			$conn->close;
+			return "No MySQL server";
+		}
+		else{
+			$sql = "DELETE FROM groups WHERE groupName = $groupName AND member = $username";
+			$conn->query($sql);
+			$conn->close();
+			return "Success";
+		}
+	
+}
+
+//Removes multiple users form a group
+function removeUsersFromGroup($groupName, array $users){
+	for($i = 0; i < count($users); i++){
+		$host = "localhost:3036";
+		$user = "root";
+		$pass = "Ubuntu14.04";
+
+		$conn = new mysqli($host,$user,$pass,"bigreddocstorage");
+
+		if($conn->connect_error){
+			$conn->close;
+			return "No MySQL server";
+		}
+		else{
+			$sql = "DELETE FROM groups WHERE groupName = $groupName AND member = $users[i]";
+			$conn->query($sql);
+			$conn->close();
+		}
+	}
+	return "Success";
 	
 }
 
