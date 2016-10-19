7fe68c46a9166e9b8aa2ce071508f14b9ea2e989
mbutera11 <michael.butera11@gmail.com>
2016-10-19 15:31:26 -0500

    Updated to determine where the files are located, depending on groups or personal

--- delete.php
+++ delete.php
@@ -1,9 +1,15 @@
 <?php
     session_start();
-    //$userName = "michael_butera";
-    $userName = $_SESSION['name_user'];
-    $target_dir = "/var/www/html/". $userName . "/";
-    $file = basename($_GET['file']); //$file becomes the name of the file clicked to download with extension
+    $userName = $_SESSION['last'];
+    if($_SESSION['lastType'] == "group"){
+        $target_dir = "/var/www/html/" . $userName . "/";
+        //echo "group";
+    }
+    else {
+        $target_dir = "/var/www/html/users/" . $userName . "/";
+        //echo "person";
+    }
+    $file = $_GET["file"]; //$file becomes the name of the file clicked to download with extension
     $file_path = $target_dir . $file; //exact path of the file
     if(!$file) {
         die ('file not found');
