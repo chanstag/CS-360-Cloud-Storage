d99914bc0d0eb237de3ebe9d10465798e4412e47
mbutera11 <michael.butera11@gmail.com>
2016-10-19 15:38:02 -0500

    Updated to determine where the files are located, depending on groups or personal and...
    It has been updated to where if upload file button is clicked before anything has been selected, it will pop up an error and reload the page. Also fixes the issue of uploading a file with an apostrophe. It now just removes them and stores them on the server clean.

--- upload.php
+++ upload.php
@@ -1,37 +1,51 @@
 
 <?php
     session_start();
-    //$userName = "michael_butera";  //hardcoded my username
-    $userName = $_SESSION['name_user'];
-    $target_dir = "/var/www/html/" . $userName . "/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $isUploaded = 1;
-
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "The file already exists.<br/>";
-        $isUploaded = 0;
+    //if there was no file selected, it prompts an alert and reloads the page
+    if(!isset($_FILES['fileToUpload']) || $_FILES['fileToUpload']['error'] == UPLOAD_ERR_NO_FILE) {
+        header('Refresh:0; url=index.php');
+        echo '<script type="text/javascript">
+                  alert("Please Select a File");
+              </script>';
     }
-    // Check file size, limeted to 50MB
-    if ($_FILES["fileToUpload"]["size"] > 50000000) {
-        echo "Your file is too large.<br/>";
-        $isUploaded = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($isUploaded == 0) {
-        echo "Your file was not uploaded.<br/>";
-    // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-            echo "There was an error uploading your file.<br/>";
+    else {
+        $userName = $_SESSION['last'];
+        if($_SESSION['lastType'] == "group") {
+                $target_dir = "/var/www/html/" . $userName . "/";
         }
-    }
-    //if the files was uploaded ($isUploaded == 1) then redirect to the index page
-    if($isUploaded == 1) {
-        header("Location: index.php"); //redirect to page after file is uploaded
-    } else 
-        echo "Your file was not uploaded successfully.";
+        else {
+                $target_dir = "/var/www/html/users/" . $userName . "/";
+        }
+        $file = basename($_FILES["fileToUpload"]["name"]);
+        $cleanFile = preg_replace('/[^A-Za-z0-9\-\(\)\. ]/', '', $file);
+        $target_file = $target_dir . $cleanFile;
+        $isUploaded = 1;
 
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "The file already exists.<br/>";
+            $isUploaded = 0;
+        }
+        // Check file size, limeted to 50MB
+        if ($_FILES["fileToUpload"]["size"] > 50000000) {
+            echo "Your file is too large.<br/>";
+            $isUploaded = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($isUploaded == 0) {
+            echo "Your file was not uploaded.<br/>";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            } else {
+                echo "There was an error uploading your file.<br/>";
+            }
+        }
+        //if the files was uploaded ($isUploaded == 1) then redirect to the index page
+        if($isUploaded == 1) {
+            header("Location: index.php"); //redirect to page after file is uploaded
+        } else 
+            echo "Your file was not uploaded successfully.";
+    } 
 ?>
