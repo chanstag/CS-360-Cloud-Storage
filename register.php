701f9a972724d2c612b64a3489ad576b94466b89
Dustin Avery <Dustin Avery>
2016-10-21 14:12:05 -0500

    Trying to fix register

--- register.php
+++ register.php
@@ -6,7 +6,7 @@ $userName = $_POST['user'];
 $pass = $_POST['pass'];
 $result = register($userName,$pass);
 if($result === "Success"){
-	mkdir("users/".$userName);
+	mkdir("var/www/html/users/".$userName);
 	session_start();
 	$_SESSION['name_user'] = $userName;
 	$_SESSION['last'] = $userName;
