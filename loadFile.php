b74925e56958c5a8f0c95c0e7bbefe4d7dc3baf0
mbutera11 <antonio.chavez629@topper.wku.edu>
2016-10-20 20:50:45 -0500

    fixed small problem

--- loadFile.php
+++ loadFile.php
@@ -9,7 +9,7 @@ $path = "/var/www/html/users/".$name."/";
 $fileSize;
 $date;
 if($type == "group"){
-	$path = "/var/www/html/".$name;
+	$path = "/var/www/html/".$name."/";
 	echo  "<div class='menuBox' style='height:10px'><div class='smaller left'><div class='button right' style='width:130px' onclick='search(\"$path\")'>Add Member</div></div>
 	<div class='right smaller'>
 				<div class='button right' style='width:130px' onclick='groupRemove(\"$path\")'>Remove Group</div>
