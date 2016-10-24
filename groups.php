c29e9b59ae3eefea1e2143aa9d7490d86bd011fc
Dustin Avery <Dustin Avery>
2016-10-24 14:50:29 -0500

    group testing

--- groups.php
+++ groups.php
@@ -13,12 +13,13 @@ if($conn->connect_error){
 	$conn->close;
 	echo "No MySQL server";
 }
-
+echo "This is sorta working";
 $result = $conn->query("SELECT (groupName) FROM groups WHERE members = '$name'");
-
+echo "This should be working";
 
 $returnString += "<div class='menuBox'><div width='50'><div class='button right' onclick='groupCreate()'>Create</div>Enter Group Name: <input type='text' id='groupName'></div></div>";
-
+echo "This is bullshit";
+echo $returnString;
 if($result->num_rows > 0){
 	while($row = $result->fetch_assoc()){
 	
