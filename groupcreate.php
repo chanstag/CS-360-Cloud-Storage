7756b8e2d23fddeff70721881f244135cfc760f8
chanstag <chandler.staggs945@topper.wku.edu>
2016-12-01 20:45:39 -0600

    Update groupcreate.php

--- groupcreate.php
+++ groupcreate.php
@@ -1,9 +1,13 @@
 <?php
 
 session_start();
-$name = $_SESSION['name_user'];
+$name = $_POST['member'];
 $newGroup = $_POST['newGroup'];
 include 'MySQL_functions.php';
+if(strpos($name, ">") || strpos($name, "<")||strpos($newGroup, ">" || strpos($newGroup,"<")){
+    echo "Sorry please try again with no characters like < or >";
+}
+else{
 echo makeGroup($newGroup,$name);
-
+}
 ?>
