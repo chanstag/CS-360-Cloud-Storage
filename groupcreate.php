f6174613ed51ebbffd590da760ddb53567180912
ACivilizedIdiot <dustin.avery889@topper.wku.edu>
2016-12-02 10:13:11 -0600

    Fix for group uploads

--- groupcreate.php
+++ groupcreate.php
@@ -4,7 +4,7 @@ session_start();
 $name = $_POST['member'];
 $newGroup = $_POST['newGroup'];
 include 'MySQL_functions.php';
-if(strpos($name, ">") || strpos($name, "<")||strpos($newGroup, ">" || strpos($newGroup,"<")){
+if(strpos($name, ">") || strpos($name, "<")||strpos($newGroup, ">" || strpos($newGroup,"<") || strpos($name, "$") || strpos($newGroup, "$")){
     echo "Sorry please try again with no characters like < or >";
 }
 else{
